df402f201c900f3b0d866cc67c1bd92b4309b3d5
NoahTN <ntolentino-nguyen@csumb.edu>
2021-03-02 14:52:43 -0800

    Add context menu function

---
+++ public/app/background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(function() {
+   chrome.contextMenus.create({
+      "id": "my-menu",
+      "title": "Search Jisho",
+      "contexts": ["selection"]
+   });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+   if (info.menuItemId === "my-menu") {
+      console.log(info.selectionText);
+      console.log(info);
+   }
+});  
