69d9b97e1eb76e52e719e0cccfba8ba1a65b56d4
NoahTN <ntolentino-nguyen@csumb.edu>
2021-08-29 23:27:50 -0700

    Update to load after window loads

--- src/content.js
+++ src/content.js
@@ -1,26 +1,28 @@
 import * as JIFrame from './jisho-iframe';
 
-chrome.storage.local.get(["width", "height"], function(data) {
-   JIFrame.insert(data.width, data.height);
- });
+window.onload = function() {
+   chrome.storage.local.get(["width", "height"], function(data) {
+      JIFrame.insert(data.width, data.height);
+   });
 
-chrome.runtime.onMessage.addListener(
-   (request, sender, sendResponse) => {
-      if(request.type == "search") {
-         JIFrame.search(request.data);
-         JIFrame.display();
+   chrome.runtime.onMessage.addListener(
+      (request, sender, sendResponse) => {
+         if(request.type == "search") {
+            JIFrame.search(request.data);
+            JIFrame.display();
+         }
+         else if(request.type == "resz-w") {
+            JIFrame.resizeWidth(request.data)
+         }
+         else {
+            JIFrame.resizeHeight(request.data)
+         }
       }
-      else if(request.type == "resz-w") {
-         JIFrame.resizeWidth(request.data)
-      }
-      else {
-         JIFrame.resizeHeight(request.data)
-      }
-   }
-);
+   );
 
-document.addEventListener('click', function(e) {
-   if (JIFrame.displaying && !document.getElementById('jiframe').contains(e.target)){
-      JIFrame.hide()
-   }
-});
+   document.addEventListener('click', function(e) {
+      if (JIFrame.displaying && !document.getElementById('jiframe').contains(e.target)){
+         JIFrame.hide()
+      }
+   });
+}
